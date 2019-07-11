72e530488067fa90bf6befa1bf337b931c802b2e
dmitrii <dimaseletkov@gmail.com>
2019-07-11 13:57:09 +0200

    created manager class

---
+++ src/Manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dmitrii on 11.07.19.
+//
+
+#include "Manager.h"
---
+++ src/Manager.h
@@ -0,0 +1,15 @@
+//
+// Created by dmitrii on 11.07.19.
+//
+
+#ifndef ALEXLENS_MANAGER_H
+#define ALEXLENS_MANAGER_H
+
+
+class Manager {
+
+
+};
+
+
+#endif //ALEXLENS_MANAGER_H
