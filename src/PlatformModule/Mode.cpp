e3659325b131353f5f687a1c43142cac264d88c3
FriedemannClaus <FriedemannClaus@users.noreply.github.com>
2019-07-14 01:15:10 +0200

    Found best OpenCL Kernels for Convolution and deleted Mode.cpp
    Co-Authored-By: yayapa <yayapa@users.noreply.github.com>
    Co-Authored-By: Jakub Trzciński <jakubtrzcskni@users.noreply.github.com>

--- src/PlatformModule/Mode.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Friedemann on 12.07.2019.
-//
-
-#include "Mode.h"
-
-Mode::Mode() {
-}
-
--- src/PlatformModule/OpenCL.h
+++ src/PlatformModule/OpenCL.h
@@ -4,6 +4,7 @@
  * @version 1.2
  */
 
+// Best GPU kernel for convolution, and also naive Kernel possibly for CPU: https://www.evl.uic.edu/kreda/gpu/image-convolution/
 
 #ifndef _OPENCL_H
 #define _OPENCL_H
