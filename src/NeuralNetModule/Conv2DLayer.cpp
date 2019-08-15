7127f87d66401b093d471b161294dd9f31fd3137
FriedemannClaus <friedemannclaus@users.noreply.github.com>
2019-08-15 15:48:50 +0200

    Zero-Padding adjusted for 3D-Matrices

--- src/NeuralNetModule/Conv2DLayer.cpp
+++ src/NeuralNetModule/Conv2DLayer.cpp
@@ -26,20 +26,25 @@ const Layer::ThreeDMatrix& Conv2DLayer::forward(const Layer::ThreeDMatrix &input
 }
 
 const Layer::ThreeDMatrix& Conv2DLayer::zeroPad(const Layer::ThreeDMatrix &input) {
-//    int zeroPadWidth = FILTER_SIZE / 2; //div
-//    int sideLength = input.cols() + 2 * zeroPadWidth;
-//    Matrix output = Layer::Matrix::Zero();
-//    output.resize(sideLength);
-//
-//
-//    //In Eigen, iterating firstly over the columns and then over the rows is more efficient.
-//    for (int j = 0; j < input.cols(); j++) {
-//        for (int i = 0; i < input.row(); i++) {
-//            output(i + zeroPadWidth, j + zeroPadWidth) = input(i, j);
-//        }
-//    }
-//    input = output;
-      return input;
+    int zeroPadWidth = FILTER_SIZE / 2; //div
+    int sideLength = input(0).cols() + 2 * zeroPadWidth;
+    static Layer::ThreeDMatrix output; // TODO, static because of returning the local variable. There should be a better way.
+    output.resize(input.rows());
+
+
+    int oldSideLength = input(0).cols();
+    for (int depth = 0; depth < input.rows(); ++depth) {
+        output(depth).resize(sideLength, sideLength);
+        output(depth).setZero(); //TODO should work
+
+        //In Eigen, iterating firstly over the columns and secondly over the rows is more efficient.
+        for (int j = 0; j < oldSideLength; j++) {
+            for (int i = 0; i < oldSideLength; i++) {
+                output(depth)(i + zeroPadWidth, j + zeroPadWidth) = input(depth)(i, j);
+            }
+        }
+    }
+    return output;
 }
 
 void Conv2DLayer::setWeights(const Layer::FourDMatrix &weights, const Layer::Vector &bias) {
