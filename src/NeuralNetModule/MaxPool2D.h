cc912479f5f48f44c157473dafa0d3ebb01faad1
Viet Pham <viet.pham@outlook.de>
2019-08-16 17:24:47 +0200

    Fixes in MaxPool2D.h

--- src/NeuralNetModule/MaxPool2D.h
+++ src/NeuralNetModule/MaxPool2D.h
@@ -25,9 +25,9 @@ private:
 
     float getMaxFloat(Vector *floats) {
         float result = (*floats)(0);
-        int numPoolElements = poolWidht * poolWidth;
+        int numPoolElements = poolNumCols * poolNumRows;
         for (int i = 0; i < (numPoolElements - 1); i++) {
-            if ((*floats)(i + 1) > result) result = (*floats)(i + 1));
+            if ((*floats)(i + 1) > result) result = (*floats)(i + 1);
         }
         return result;
     }
@@ -37,7 +37,7 @@ public:
     MaxPool2D(const int inputWidth, const int inputHeight, const int inputChannels, const int poolWidth, const int poolHeight, const int poolStride):
     //Initialisierungsliste
 
-            Layer(inputWidth * inputHeight * inputChannels, (((inputWidth - poolWidth) / poolStride) + 1) * (((inputHeight - poolHeight) / poolStride) + 1) * inputChannels),
+            //Layer(inputWidth * inputHeight * inputChannels, (((inputWidth - poolWidth) / poolStride) + 1) * (((inputHeight - poolHeight) / poolStride) + 1) * inputChannels),
             inputNumRows(inputHeight),
             inputNumCols(inputWidth),
             inputNumChannels(inputChannels),
@@ -50,14 +50,14 @@ public:
 
      ThreeDMatrix forward(const ThreeDMatrix &inputMatrix) {
         outputMatrix.resize(outputNumRows, outputNumCols);
-        int outputSize = ((inputChannelNumCols - poolWidth) / poolStride) + 1; //size of one side!
-        int numPoolElements = poolWidht * poolWidth;
+        int outputSize = ((inputNumCols - poolNumCols) / stride) + 1; //size of one side!
+        int numPoolElements = poolNumCols * poolNumRows;
         for (int c = 0; c < inputNumChannels; c++) { // Iterieren ueber Channel
             for (int i = 0; i < outputSize; i++) { // Iterieren ueber Zeilen des Outputs
                 for (int j = 0; j < outputSize; j++) { // Iterieren ueber Spalten des Outputs
                     Vector floats;
                     for (int k = 0; k < numPoolElements; k++) {
-                        floats(k) = (*inputMatrix)(c)((i * stride) + (k / 3), (j * stride) + (k % 3));
+                        floats(k) = (inputMatrix)(c)((i * stride) + (k / 3), (j * stride) + (k % 3));
                     }
                     float max = getMaxFloat(&floats);
                     outputMatrix(c)(i, j) = max;
