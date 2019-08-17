ad830a5a62c8fe45f8cbc53ef8fe93f7fdf4634d
Viet2503 <viet.pham@outlook.de>
2019-08-17 09:52:59 +0200

    Fixed MaxPoolLayer:
    Calculation of outputNumRows and outputNumCols was wrong.
    Forgot to resize Vector floats.

--- src/NeuralNetModule/MaxPool2D.h
+++ src/NeuralNetModule/MaxPool2D.h
@@ -43,8 +43,8 @@ public:
             inputNumChannels(inputChannels),
             poolNumRows(poolHeight),
             poolNumCols(poolWidth),
-            outputNumRows(inputNumRows / poolNumRows),
-            outputNumCols(inputNumCols / poolNumCols),
+            outputNumRows(((inputNumRows - poolNumRows) / poolStride) + 1),
+            outputNumCols(((inputNumCols - poolNumCols) / poolStride) + 1),
             stride(poolStride)
     {}
 
@@ -52,17 +52,25 @@ public:
         assert(inputMatrix.rows() == inputNumChannels);
         assert(inputMatrix(0).cols() == inputNumCols);
         assert(inputMatrix(0).rows() == inputNumRows);
-        outputMatrix.resize(outputNumRows, outputNumCols);
+        int outputNumChannels = inputNumChannels;
+
+        outputMatrix.resize(inputNumChannels);
+        for (int i = 0; i < inputNumChannels; i++) {
+            outputMatrix(i).resize(outputNumRows, outputNumCols);
+        }
+
         int outputSize = ((inputNumCols - poolNumCols) / stride) + 1; //size of one side!
         int numPoolElements = poolNumCols * poolNumRows;
         for (int c = 0; c < inputNumChannels; c++) { // Iterieren ueber Channel
             for (int i = 0; i < outputSize; i++) { // Iterieren ueber Zeilen des Outputs
                 for (int j = 0; j < outputSize; j++) { // Iterieren ueber Spalten des Outputs
                     Vector floats;
+                    floats.resize(numPoolElements);
                     for (int k = 0; k < numPoolElements; k++) {
-                        floats(k) = (inputMatrix)(c)((i * stride) + (k / 3), (j * stride) + (k % 3));
+                        floats(k) = (inputMatrix)(c)((i * stride) + (k / 3), (j * stride) + (k % 3)); //Fehler
                     }
                     float max = getMaxFloat(&floats);
+                    cout << "max: " << max << endl;
                     outputMatrix(c)(i, j) = max;
                 }
             }
