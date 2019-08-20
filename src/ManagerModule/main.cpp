2bae0a96108b832b098acb4d8052cd4fdf3a2ca0
Viet Pham <viet.pham@outlook.de>
2019-08-20 21:33:54 +0200

    Fixed wrong classification results when using NCSs (Manhole cover...). See: https://doc.qt.io/qt-5/qcoreapplication.html#locale-settings, https://forum.qt.io/topic/98175/intel-openvino-on-linux/4

--- src/ManagerModule/main.cpp
+++ src/ManagerModule/main.cpp
@@ -7,6 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    setlocale(LC_NUMERIC,"C");
     Subject* subject = new Subject();
     Manager* manager = new Manager(subject);
     MainWindow* w = new MainWindow(manager, subject);
