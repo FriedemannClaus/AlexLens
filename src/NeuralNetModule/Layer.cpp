b4adc0fa0328c63b40d1c905a60b348ae38cd20a
Viet Pham <viet.pham@outlook.de>
2019-08-25 14:08:31 +0200

    Added class Layer

---
+++ src/NeuralNetModule/Layer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Viet Pham.
+//
+
+#include "Layer.h"
---
+++ src/NeuralNetModule/Layer.h
@@ -0,0 +1,21 @@
+//
+// Created by Viet Pham.
+//
+
+#ifndef ALEXLENS_LAYER_H
+#define ALEXLENS_LAYER_H
+
+
+#include <Eigen/Core>
+
+typedef Eigen::Matrix<float, Eigen::Dynamic, 1> Vector;
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+typedef Eigen::Matrix<Matrix, Eigen::Dynamic, 1> ThreeDMatrix;
+typedef Eigen::Matrix<Matrix, Eigen::Dynamic, Eigen::Dynamic> FourDMatrix;
+
+class Layer {
+    virtual void forward(ThreeDMatrix &input, ThreeDMatrix &output) = 0;
+};
+
+
+#endif //ALEXLENS_LAYER_H
