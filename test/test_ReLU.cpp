e1a5c8be0ffe9d50b51a9fd1980e8f75d2ad0501
Viet Pham <viet.pham@outlook.de>
2019-08-26 23:39:55 +0200

    Fixed needed Eigen include needed to test ReLU Layer.

--- test/test_ReLU.cpp
+++ test/test_ReLU.cpp
@@ -1,7 +1,7 @@
 //
 // Created by viet on 25.08.19.
 //
-
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 TEST_CASE( "inputMatrix size equals outputMatrix size") {
