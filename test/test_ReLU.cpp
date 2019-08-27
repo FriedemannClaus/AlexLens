8d98d575f8565ab1d7ccf4132fb1c9472eda102a
Viet Pham <viet.pham@outlook.de>
2019-08-27 02:42:40 +0200

    Implemented tests for ReLU Layer.

--- test/test_ReLU.cpp
+++ test/test_ReLU.cpp
@@ -2,10 +2,12 @@
 // Created by viet on 25.08.19.
 //
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <NeuralNetModule/ReLU.h>
 #include "catch.hpp"
 
-TEST_CASE( "inputMatrix size equals outputMatrix size") {
-    /* ThreeDMatrix testInput;
+TEST_CASE( "ReLU: inputMatrix size equals outputMatrix size", "[fast]") {
+     ThreeDMatrix testInput;
      ThreeDMatrix testOutput;
      ReLU *reLU = new ReLU();
      testInput.resize(5);
@@ -14,6 +16,54 @@ TEST_CASE( "inputMatrix size equals outputMatrix size") {
      }
 
      reLU->forward(testInput, testOutput);
-     REQUIRE(testOutput.rows() == 5);*/
-    REQUIRE(1 == 1);
+     REQUIRE(testOutput.rows() == 5);
+     REQUIRE(testOutput(0).rows() == 6);
+     REQUIRE(testOutput(0).cols() == 7);
+}
+
+TEST_CASE( "ReLU: outputMatrix contains only float scalars >= 0", "[fast]") {
+    ThreeDMatrix testInput;
+    ThreeDMatrix testOutput;
+    ReLU *reLU = new ReLU();
+    testInput.resize(1);
+    testInput(0).resize(2, 3);
+    testInput(0)(0, 0) = (float) -50.2323;
+    testInput(0)(0, 1) = (float) -0;
+    testInput(0)(0, 2) = (float) +0;
+    testInput(0)(1, 0) = (float) -0.00001;
+    testInput(0)(1, 1) = (float) +0.00001;
+    testInput(0)(1, 2) = (float) 50.2323;
+
+    reLU->forward(testInput, testOutput);
+
+    REQUIRE(testOutput(0)(0, 0) >= 0);
+    REQUIRE(testOutput(0)(0, 1) >= 0);
+    REQUIRE(testOutput(0)(0, 2) >= 0);
+    REQUIRE(testOutput(0)(1, 0) >= 0);
+    REQUIRE(testOutput(0)(1, 1) >= 0);
+    REQUIRE(testOutput(0)(1, 2) >= 0);
+}
+
+TEST_CASE( "ReLU: Activation function works correctly", "[fast][math]") {
+    ThreeDMatrix testInput;
+    ThreeDMatrix testOutput;
+    ReLU *reLU = new ReLU();
+    testInput.resize(1);
+    testInput(0).resize(2, 3);
+    testInput(0)(0, 0) = -2323.4445;
+    testInput(0)(0, 1) = -0;
+    testInput(0)(0, 2) = +0;
+    testInput(0)(1, 0) = -0.00001;
+    testInput(0)(1, 1) = 0.00001;
+    testInput(0)(1, 2) = 2323.4445;
+
+    reLU->forward(testInput, testOutput);
+
+    REQUIRE(testOutput(0)(0, 0) == (float) 0);
+    REQUIRE(testOutput(0)(0, 1) == (float) 0);
+    REQUIRE(testOutput(0)(0, 2) == (float) 0);
+    REQUIRE(testOutput(0)(1, 0) == (float) 0);
+
+    REQUIRE(testOutput(0)(1, 1) == (float) 0.00001);
+    REQUIRE(testOutput(0)(1, 2) == (float) 2323.4445);
 }
