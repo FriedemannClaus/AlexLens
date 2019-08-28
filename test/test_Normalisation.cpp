179714b1715af64286b4eafb5eb5104f2dab09ee
jakubt <jakub2trzcinski@gmail.com>
2019-08-28 13:46:29 +0200

    test_Softmax updated, test_Normalisation added, small changes in Normalisation

---
+++ test/test_Normalisation.cpp
@@ -0,0 +1,8 @@
+//
+// Created by jakubt on 28.08.19.
+//
+#define DELTA 1e-5
+#include <NeuralNetModule/Normalization.cpp>
+#include "catch.hpp"
+TEST_CASE("Normalization: Normalization function works correctly", "[fast]"){
+}
