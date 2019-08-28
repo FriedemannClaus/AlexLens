ba5e3a6151f3559bb9ebf79b6c2acd5992e2ebc2
jakubt <jakub2trzcinski@gmail.com>
2019-08-28 15:06:28 +0200

    test_Softmax, test_Normalisation updated

--- test/test_Normalisation.cpp
+++ test/test_Normalisation.cpp
@@ -1,8 +1,67 @@
 //
 // Created by jakubt on 28.08.19.
 //
-#define DELTA 1e-5
+#define DELTA 0.005
+
 #include <NeuralNetModule/Normalization.cpp>
+#include <iostream>
 #include "catch.hpp"
-TEST_CASE("Normalization: Normalization function works correctly", "[fast]"){
+
+TEST_CASE("Normalization: Normalization function works correctly", "[fast]") {
+    int hyperKNorm = 0;
+    int hyperAlpha = 1;
+    int hyperBeta = 1;
+    int hyperN = 2;
+
+    ThreeDMatrix inputMatrix;
+    ThreeDMatrix expectedOutputMatrix;
+    ThreeDMatrix outputMatrix;
+
+    int inputChannels = 4;
+    inputMatrix.resize(inputChannels);
+    for (int i = 0; i < inputChannels; i++) {
+        inputMatrix(i).resize(3, 3);
+    }
+    inputMatrix(0) << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+    inputMatrix(1) << 1, 2, 1, 2, 3, 2, 3, 4, 3;
+    inputMatrix(2) << 2, 1, 2, 3, 2, 3, 4, 3, 4;
+    inputMatrix(3) << 4, 2, 1, 5, 2, 1, 2, 2, 4;
+
+    expectedOutputMatrix.resize(inputChannels);
+    for (int i = 0; i < inputChannels; i++) {
+        expectedOutputMatrix(i).resize(3, 3);
+    }
+    expectedOutputMatrix(0)<<0.5, 0.25, 0.3, 0.2, 0.17, 0.15, 0.12, 0.1, 0.1;
+    expectedOutputMatrix(1)<<0.17, 0.22, 0.07, 0.07, 0.14, 0.04, 0.04, 0.04, 0.03;
+    expectedOutputMatrix(2)<<0.1, 0.11, 0.33, 0.08, 0.12, 0.21, 0.14, 0.1, 0.1;
+    expectedOutputMatrix(3)<<0.2, 0.4, 0.2, 0.15, 0.25, 0.1, 0.1, 0.15, 0.13;
+
+    Normalization *normalization = new Normalization(hyperKNorm, hyperAlpha, hyperBeta, hyperN);
+
+    SECTION("Output matrix size equals input matrix size") {
+        normalization->forward(inputMatrix, outputMatrix);
+        int inputDepth = inputMatrix.size();
+        REQUIRE(inputDepth == outputMatrix.size());
+        for (int i = 0; i < inputDepth; i++) {
+            REQUIRE(inputMatrix(i).rows() == outputMatrix(i).rows());
+            REQUIRE(inputMatrix(i).cols() == outputMatrix(i).cols());
+        }
+    }
+    SECTION("Output matrix is calculated correctly") {
+        normalization->forward(inputMatrix, outputMatrix);
+        int inputDepth = inputMatrix.size();
+        int inputRows = inputMatrix(0).rows();
+        int inputCols = inputMatrix(0).cols();
+        for(int i=0; i<inputDepth;i++){
+            for(int j=0;j<inputRows;j++){
+                for(int k=0;k<inputCols;k++){
+                    //REQUIRE(expectedOutputMatrix(i)(j,k) == Approx(outputMatrix(i)(j,k)).epsilon(DELTA));
+                }
+            }
+            std::cout<<"input\n"<<inputMatrix(i)<<"\n\n";
+            std::cout<<"output\n"<<outputMatrix(i)<<"\n\n";
+            std::cout<<"expected output\n"<<expectedOutputMatrix(i)<<"\n\n";
+        }
+
+    }
 }
--- test/test_Softmax.cpp
+++ test/test_Softmax.cpp
@@ -4,6 +4,8 @@
 #define DELTA 1e-5
 #include <NeuralNetModule/Softmax.cpp>
 #include "catch.hpp"
+#include <cstdlib>
+
 TEST_CASE("Softmax: correct functioning of the Softmax function", "[fast]") {
     ThreeDMatrix testInput;
     ThreeDMatrix testInput2;
@@ -12,14 +14,23 @@ TEST_CASE("Softmax: correct functioning of the Softmax function", "[fast]") {
     int testSize = 1000;
     int testSize2 = 73;
     Softmax *softmax = new Softmax();
+
     testInput.resize(1);
     testInput(0).resize(testSize, 1);
     for (int i = 0; i < testSize; i++) {
         testInput(0)(i, 0) = (float) i / 100;
     }
+    testInput2.resize(1);
+    testInput2(0).resize(testSize2, 1);
+    for (int i = 0; i < testSize2; i++) {
+        testInput2(0)(i, 0) = (float)(rand()%100)/100;
+    }
+
     SECTION("Input vector size equals output vector size") {
         softmax->apply(testInput, testOutput);
+        softmax->apply(testInput2, testOutput2);
         REQUIRE(testOutput.size() == testInput(0).rows());
+        REQUIRE(testOutput2.size() == testInput2(0).rows());
     }
     SECTION("Output vector values sum up to 1") {
         softmax->apply(testInput, testOutput);
@@ -27,10 +38,10 @@ TEST_CASE("Softmax: correct functioning of the Softmax function", "[fast]") {
         for (int i = 0; i < testOutput.size(); i++) {
             outputSum += testOutput(i);
         }
-        REQUIRE(outputSum == Approx(1.0).epsilon(DELTA));
+        REQUIRE(1.0 == Approx(outputSum).epsilon(DELTA));
     }
 }
-/*
+
 TEST_CASE("Catching lost precision during computation", "[fast]") {
     ThreeDMatrix testInput;
     Vector testOutput;
@@ -47,7 +58,6 @@ TEST_CASE("Catching lost precision during computation", "[fast]") {
         for (int i = 0; i < testOutput.size(); i++) {
             outputSum += testOutput(i);
         }
-        REQUIRE_THROWS(outputSum == Approx(1.0).epsilon(DELTA));
+        REQUIRE_THROWS(1.0 == Approx(outputSum).epsilon(DELTA));
     }
 }
-*/
