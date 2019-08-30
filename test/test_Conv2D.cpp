4ea905139489e6f2ce6f35bfbd2b56c9cd8bff9c
jakubt <jakub2trzcinski@gmail.com>
2019-08-30 13:27:24 +0200

    alexnet.labels,mapping,xml added to git tests updated

---
+++ test/test_Conv2D.cpp
@@ -0,0 +1,22 @@
+//
+// Created by jakubt on 30.08.19.
+//
+/*
+#define DELTA 0.05
+#include <NeuralNetModule/Conv2D.cpp>
+#include "catch.hpp"
+
+TEST_CASE("Convolution forward pass functions correctly", "[slow]"){
+    ThreeDMatrix inputMatrix;
+    ThreeDMatrix outputMatrix;
+
+    SECTION("Output matrix has correct size"){
+
+    }
+    SECTION("Input matrix is correctly zero-padded"){
+
+    }
+    SECTION("")
+
+
+}*/
