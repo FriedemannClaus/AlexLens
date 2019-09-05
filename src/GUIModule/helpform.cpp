67ed97db7556f3c13640530c8b1e659f887c2933
FriedemannClaus <friedemannclaus@users.noreply.github.com>
2019-09-05 22:23:21 +0200

    Corrected the help window

--- src/GUIModule/helpform.cpp
+++ src/GUIModule/helpform.cpp
@@ -14,21 +14,21 @@ HelpForm::HelpForm(QWidget *parent) :
 
     label = new QLabel(this);
     label->setText("\n    Das Produkt dient zur Bilderklassifizierung auf heterogenen Plattformen mittels neuronaler"
-                   "\nNetze u.a. AlexNet. Es soll zwischen verschiedenen Betriebsmodi ausgewählt werden können:"
-                   " \n\n     Optimal Modus: der optimale Modus, in "
+                   "\nNetze (u.a. AlexNet). Es kann zwischen verschiedenen Betriebsmodi ausgewählt werden:"
+                   " \n\n     Optimaler Modus: der optimale Modus, in "
                    "dem die CPU (im Falle AlexNet GPU) benutzt wird.\n\n     Hohe Performance: der schnellste und "
                    "performanteste Modus, in dem alle verfügbaren\nheterogenen Plattformen benutzt werden.\n\n     "
                    "Geringer Leistungsverbrauch: der Modus, in dem genau ein Intel Movidius Neural Compute\nStick benutzt wird."
-                   "\n\n     Hohe Energieefizient: der Modus, in dem alle verfügbaren Intel Movidius Neural Compute\nSticks benutzt werden."
-                   "\n\n\n     Zudem gibt es die Möglichkeit das vorhandene neuronale Netz mittels Transfer Learning zu"
+                   "\n\n     Hohe Energieeffizienz: der Modus, in dem alle verfügbaren Intel Movidius Neural Compute\nSticks benutzt werden."
+                   "\n\n\n    Zudem gibt es die Möglichkeit das vorhandene neuronale Netz mittels Transfer Learning zu"
                    "\ntrainieren.\n    Unter Transfer Learning versteht man das Übertragen der Ergebnisse eines fertig trainierten"
-                   "\nneuronalen Netzes auf eine neue Aufgabe.\n   Beim Trainieren gibt es nur einen Modus, bei dem die CPU verwendet wird. Dieser ist somit\n"
-                   " der optimale Modus. Beim Trainieren muss der Datensatz folgendendermaßen formatiert sein:"
-                   "\n     Ausgewählt wird ein Ordner, der die Ordner test, train und valid enthält. Diese wiederum"
+                   "\nneuronalen Netzes auf eine neue Aufgabe.\n    Beim Trainieren gibt es nur einen Modus, bei dem die CPU verwendet wird. Dieser ist somit\n"
+                   " der optimale Modus.\n    Der Trainingsdatensatz muss folgendendermaßen formatiert sein:"
+                   "\nAusgewählt wird ein Ordner, der die Ordner test, train und valid enthält. Diese wiederum"
                    "\n enthalten die (gleichen) Objektklassen als Ordner, welche wiederum Bilder der\n jeweiligen Objekte enthalten."
-                   "\n\n    Zur Veranschaulichung kann man sich einen Beispieldatensatz herunterladen unter"
+                   "\n\n    Zur Veranschaulichung kann man sich einen Beispieldatensatz herunterladen unter:"
                    "\nhttps://cloud.viet-pham.de/s/eKRM5JyxBpAAjEn?path=%2F"
-                   "\n\n    Der Open-Source-Code dieses Programms ist zu finden unter"
+                   "\n\n    Der Open-Source-Code dieses Programms ist zu finden unter:"
                    "\nhttps://github.com/FriedemannClaus/AlexLens");
 
     h_panel->addWidget(label);
